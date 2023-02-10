8e03eee0d8cc2e1d5fcb70229e543fbbf08cb3c6
Vitali Lapeka <lapeko88@gmail.com>
2023-02-10 16:31:04 +0100

    create UserList component

---
+++ users/src/components/UserList.jsx
@@ -0,0 +1,25 @@
+const UserList = ({ users, loading }) => {
+  if (loading) return <h2>Loading...</h2>;
+  return (
+    <table className="table">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Name</th>
+          <th scope="col">Email</th>
+        </tr>
+      </thead>
+      <tbody>
+        {users.map((user, index) => (
+          <tr key={user.id}>
+            <th scope="row">{index + 1}</th>
+            <td>{user.name}</td>
+            <td>{user.email}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default UserList;
