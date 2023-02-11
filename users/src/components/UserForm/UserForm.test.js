7ef2f7731a60bcf127232cee668fd5f1d6b6f912
Vitali Lapeka <lapeko88@gmail.com>
2023-02-11 12:23:15 +0100

    add new test case

--- users/src/components/UserForm/UserForm.test.js
+++ users/src/components/UserForm/UserForm.test.js
@@ -11,4 +11,26 @@ describe("UserForm component", () => {
     expect(inputs).toHaveLength(2);
     expect(submit).toBeDisabled();
   });
+
+  it("should call addNewUser from props if user filled name + email and pressed submit", async () => {
+    const addNewUser = jest.fn();
+    const { user, getByRole, getAllByRole } = setup(
+      <UserForm addNewUser={addNewUser} loading={false} />
+    );
+
+    const [nameInput, emailInput] = getAllByRole("textbox");
+    const submit = getByRole("button", { name: "Submit" });
+
+    await user.click(nameInput);
+    await user.keyboard("First name");
+    await user.click(emailInput);
+    await user.keyboard("test@mail.com");
+    await user.click(submit);
+
+    expect(addNewUser).toHaveBeenCalledWith({
+      id: expect.any(Number),
+      name: "First name",
+      email: "test@mail.com",
+    });
+  });
 });
