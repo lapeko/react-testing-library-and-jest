39b46b3318a8a8f1314705fd63b459bbb77767dc
Vitali Lapeka <lapeko88@gmail.com>
2023-02-12 13:13:13 +0100

    add custom jest matcher

--- users/src/setupTests.js
+++ users/src/setupTests.js
@@ -2,4 +2,26 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+import "@testing-library/jest-dom";
+
+expect.extend({
+  willNotAppearInTheDom: async (findByQuery) => {
+    try {
+      await findByQuery;
+      return {
+        pass: false,
+        message: () =>
+          `Your element(s) which should not appear in the DOM suddenly found there`,
+      };
+    } catch (e) {
+      if (/^found multiple elements/i.test(e.message))
+        return {
+          pass: false,
+          message: () => e.message,
+        };
+      return {
+        pass: true,
+      };
+    }
+  },
+});
