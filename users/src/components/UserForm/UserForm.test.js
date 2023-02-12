4f3e11140d96275705aeefb0bb5d75ad796012c3
Vitali Lapeka <lapeko88@gmail.com>
2023-02-12 01:08:57 +0100

    remove describe blocks

--- users/src/components/UserForm/UserForm.test.js
+++ users/src/components/UserForm/UserForm.test.js
@@ -1,43 +1,41 @@
 import { setup } from "../../testUtils";
 import UserForm from "./UserForm";
 
-describe("UserForm component", () => {
-  it("should show two inputs and a submit button", () => {
-    const { getByRole, getAllByRole } = setup(<UserForm />);
+it("should show two inputs and a submit button", () => {
+  const { getByRole, getAllByRole } = setup(<UserForm />);
 
-    const inputs = getAllByRole("textbox");
-    const submit = getByRole("button", { name: "Submit" });
+  const inputs = getAllByRole("textbox");
+  const submit = getByRole("button", { name: "Submit" });
 
-    expect(inputs).toHaveLength(2);
-    expect(submit).toBeDisabled();
+  expect(inputs).toHaveLength(2);
+  expect(submit).toBeDisabled();
+});
+
+it("should call addNewUser from props and clean form if user filled name + email and pressed submit", async () => {
+  const addNewUser = jest.fn();
+  const { user, getByRole, getByLabelText } = setup(
+    <UserForm addNewUser={addNewUser} loading={false} />
+  );
+
+  const nameLabel = getByLabelText("Name");
+  const emailLabel = getByLabelText("Email");
+  const submit = getByRole("button", { name: "Submit" });
+
+  await user.click(nameLabel);
+  await user.keyboard("First name");
+  await user.click(emailLabel);
+  await user.keyboard("test@mail.com");
+  await user.click(submit);
+
+  expect(addNewUser).toHaveBeenCalledWith({
+    id: expect.any(Number),
+    name: "First name",
+    email: "test@mail.com",
   });
 
-  it("should call addNewUser from props and clean form if user filled name + email and pressed submit", async () => {
-    const addNewUser = jest.fn();
-    const { user, getByRole, getByLabelText } = setup(
-      <UserForm addNewUser={addNewUser} loading={false} />
-    );
+  const nameInput = getByRole("textbox", { name: "Name" });
+  const emailInput = getByRole("textbox", { name: "Email" });
 
-    const nameLabel = getByLabelText("Name");
-    const emailLabel = getByLabelText("Email");
-    const submit = getByRole("button", { name: "Submit" });
-
-    await user.click(nameLabel);
-    await user.keyboard("First name");
-    await user.click(emailLabel);
-    await user.keyboard("test@mail.com");
-    await user.click(submit);
-
-    expect(addNewUser).toHaveBeenCalledWith({
-      id: expect.any(Number),
-      name: "First name",
-      email: "test@mail.com",
-    });
-
-    const nameInput = getByRole("textbox", { name: "Name" });
-    const emailInput = getByRole("textbox", { name: "Email" });
-
-    expect(nameInput).toBeEmptyDOMElement();
-    expect(emailInput).toBeEmptyDOMElement();
-  });
+  expect(nameInput).toBeEmptyDOMElement();
+  expect(emailInput).toBeEmptyDOMElement();
 });
--- users/src/components/UserList/UserList.test.js
+++ users/src/components/UserList/UserList.test.js
@@ -2,25 +2,23 @@ import { within } from "@testing-library/react";
 import { setup } from "../../testUtils";
 import UserList from "./UserList";
 
-describe("UserList component", () => {
-  it("should render one user in a table if received number of users = 1", () => {
-    const users = [{ id: 1, name: "Test", email: "email@mail.com" }];
-    const { getByTestId } = setup(<UserList users={users} loading={false} />);
+it("should render one user in a table if received number of users = 1", () => {
+  const users = [{ id: 1, name: "Test", email: "email@mail.com" }];
+  const { getByTestId } = setup(<UserList users={users} loading={false} />);
 
-    const rowsWithUser = within(getByTestId("users")).getAllByRole("row");
-    expect(rowsWithUser).toHaveLength(1);
-  });
+  const rowsWithUser = within(getByTestId("users")).getAllByRole("row");
+  expect(rowsWithUser).toHaveLength(1);
+});
 
-  it("should render name and email of every user received from props", () => {
-    const users = [
-      { id: 1, name: "Name 1", email: "email1@qwe.qw" },
-      { id: 2, name: "Name 2", email: "email2@qwe.qw" },
-    ];
-    const { getByRole } = setup(<UserList users={users} loading={false} />);
+it("should render name and email of every user received from props", () => {
+  const users = [
+    { id: 1, name: "Name 1", email: "email1@qwe.qw" },
+    { id: 2, name: "Name 2", email: "email2@qwe.qw" },
+  ];
+  const { getByRole } = setup(<UserList users={users} loading={false} />);
 
-    users.forEach(({ name, email }) => {
-      expect(getByRole("cell", { name })).toBeInTheDocument();
-      expect(getByRole("cell", { name: email })).toBeInTheDocument();
-    });
+  users.forEach(({ name, email }) => {
+    expect(getByRole("cell", { name })).toBeInTheDocument();
+    expect(getByRole("cell", { name: email })).toBeInTheDocument();
   });
 });
